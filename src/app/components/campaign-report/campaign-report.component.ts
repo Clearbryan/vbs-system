7d326814db8de5e43b204f7e3588f4d2338ecf2f
Clearbryan <cchetekwe@gmail.com>
2020-08-25 09:32:45 +0200

    Changed title

--- src/app/components/campaign-report/campaign-report.component.ts
+++ src/app/components/campaign-report/campaign-report.component.ts
@@ -33,53 +33,55 @@ export class CampaignReportComponent implements OnInit {
     this.activeRoute.params.subscribe((params: Params) => {
       this.reportId = Number(params.id)
     })
-
-    this.reportService.getSingleReport(this.reportId).subscribe((data: any) => {
-      // this.active = true
-      console.log(data)
-      data.start_date = new Date(data.start_date).toDateString()
-      this.report = data
-
-      setInterval(() => {
+    setInterval(() => {
+      this.reportService.getSingleReport(this.reportId).subscribe((data: any) => {
+        // this.active = true
+        console.log(data)
+        data.start_date = new Date(data.start_date).toDateString()
         this.report = data
-
-        this.piechart = new Chart(`${this.pieChartId}`, {
-          
-          type: 'bar',
-          data: {
-            labels: [`Calls`, 'Replies'],
-            datasets: [{
-              label: '# of Replies',
-              data: [data.calls, data.replies],
-              backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 99, 132, 0.2)'
-    
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)'
-              ],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
+  
+        
+          this.report = data
+  
+          this.piechart = new Chart(`${this.pieChartId}`, {
+            
+            type: 'line',
+            data: {
+              labels: [`Calls`, 'Replies'],
+              datasets: [{
+                label: '# of Replies',
+                data: [
+                  [data.calls, data.progress],
+                  [10, 45]
+                ],
+                backgroundColor: [
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 99, 132, 0.2)'
+      
+                ],
+                borderWidth: 1
               }]
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              }
             }
-          }
-        })
-   }, 10000)
-   
-    }, error => {
-        // handle error
-        this.active = false
-        console.log(error)
-    })
+          })
+     
+     
+      }, error => {
+          // handle error
+          this.active = false
+          console.log(error)
+      })
+     }, 5000)
+
+ 
   }
 
  
